f2a1711d5fd1792fd508a14031dd6e47279379e0
igli <iglidraci98@gmail.com>
2022-06-02 02:04:36 +0200

    strategy pattern and named tuples

---
+++ Functional_Programming/functions_iterators_generators/string.py
@@ -0,0 +1,13 @@
+from decimal import Decimal
+from typing import Optional
+
+
+def remove(text: str, chars: str) -> str:
+    '''
+    This function will recursively remove each of the characters from chars
+    '''
+    if chars:
+        return remove(text.replace(chars[0], ""), chars[1:])
+    return text
+
+print(remove("Hello $$ World ??", "$?"))
